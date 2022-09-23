450b8fe091ad63443058abd72eb94050b32563fd
tonimjdev <tonimdev@gmail.com>
2022-09-23 11:43:49 +0200

    Creación mini calculadora

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # MiniCalculadora
 
+Proyecto para practicar ANGULAR creando una mini calculadora.
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.3.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mini-calculadora';
+  titulo = 'Mini Calculadora';
+
+  n1: number = 0;
+  n2: number = 0;
+  resultado: number = 0;
+  alerta: boolean = false;
+
+  sumar(): void {
+    this.alerta=false;
+    this.resultado = this.n1+this.n2;
+  }
+  restar(): void {
+    this.alerta=false;
+    this.resultado = this.n1-this.n2;
+  }
+  multiplicar(): void {
+    this.alerta=false;
+    this.resultado = this.n1*this.n2;
+  }
+  dividir(): void {
+    if (this.n2===0) {this.alerta = true;}
+    else this.resultado = this.n1/this.n2;
+  }
+
+
 }
